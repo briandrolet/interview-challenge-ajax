8eea9e70f62d0d0e604072ef8c05e9e729b17256
Brian Drolet <brian@pixelunion.net>
2018-12-19 19:02:16 -0700

    create divs to view data returned from profiles.php

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ window.setTimeout(get_facebook_profiles, 400);
 window.setTimeout(get_twitter_profiles, 800);
 
 // Keep this function.
-function get_facebook_profiles()
-{
-	$.get('/profiles.php?enabled=1&network=facebook&fields=name,network', function(data) {
-		// Create <div>'s from the data, add the class "profile" and "facebook", and set the inner text to the name.
-		// It'll look like <div class="profile facebook">Matt</div>
-		// Add these new divs as children of the .facebook_list div.
-		// Then remove the "Loading..." text.
-	});
+function get_facebook_profiles() {
+  $.get("/profiles.php?enabled=1&network=facebook&fields=name,network", function(data) {
+    if (data) {
+      $("div .facebook_list")
+        .find(".loading")
+        .remove();
+      display_profiles("facebook", $.parseJSON(data));
+    }
+  });
 }
 
 // Keep this function.
-function get_twitter_profiles()
-{
-	$.get('/profiles.php?enabled=1&network=twitter&fields=name,network', function(data) {
-		// Create <div>'s from the data, add the class "profile" and "twitter", and set the inner text to the name.
-		// It'll look like <div class="profile twitter">Matt</div>
-		// Add these new divs as children of the .twitter_list div.
-		// Then remove the "Loading..." text.
-	});
+function get_twitter_profiles() {
+  $.get("/profiles.php?enabled=1&network=twitter&fields=name,network", function(data) {
+    if (data) {
+      $("div .twitter_list")
+        .find(".loading")
+        .remove();
+      display_profiles("twitter", $.parseJSON(data));
+    }
+  });
+}
+
+//helper function to create and display the profile div tag for each platform
+function display_profiles(network, profiles) {
+  $.each(profiles, function(index, profile) {
+    $profile_div = '<div class="profile ' + network + '">' + profile.name + "</div>";
+    $("div ." + network + "_list").append($profile_div);
+  });
 }
