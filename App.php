99f0016758af7fb8816fa79c81c854fe73a6f7ff
Brian Drolet <brian@pixelunion.net>
2018-12-19 19:04:02 -0700

    reformat App.php

--- App.php
+++ App.php
@@ -3,30 +3,28 @@
 class App
 {
 
-	/**
-	 * The app version.
-	 * 
-	 * @return float
-	 */
-	public function getVersion()
-	{
-		return 1.2;
-	}
+    /**
+     * The app version.
+     *
+     * @return float
+     */
+    public function getVersion()
+    {
+        return 1.2;
+    }
 
-	/**
-	 * Tries to get a request variable. If not set, returns null.
-	 *
-	 * @param $name
-	 * @return string
-	 */
-	public function getRequestVar($name): ?string
-	{
-		if (isset($_REQUEST[$name]))
-		{
-			return $_REQUEST[$name];
-		}
+    /**
+     * Tries to get a request variable. If not set, returns null.
+     *
+     * @param $name
+     * @return string
+     */
+    public function getRequestVar($name): ?string
+    {
+        if (isset($_REQUEST[$name])) {
+            return $_REQUEST[$name];
+        }
 
-		return null;
-	}
-
+        return null;
+    }
 }
